751f6b01e884207bba71f7a734e3f23003f9fd9a
n-kev1n <kevin_@tuta.io>
2024-01-24 19:57:07 +0100

    imported fetchOutlet hook to generate lazy loading as well as fetched outlets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import Navbar from "@/components/navbar";
+import useFetchOutlets from "./hooks/api/fetch-outlets";
+import Loader from "./components/loader";
 
 function App() {
+  const { outlets, isLoading } = useFetchOutlets();
+
+  if (outlets) console.log(outlets);
+
   return (
     <>
       <div className="bg-slate-50">
         <Navbar />
+
+        {isLoading ? <Loader /> : null}
       </div>
     </>
   );
