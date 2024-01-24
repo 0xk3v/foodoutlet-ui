461dbe8e2e1d546674710763fdc5d11943cb7ecc
n-kev1n <kevin_@tuta.io>
2024-01-24 23:05:28 +0100

    changed body background

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import "./App.css";
 import Navbar from "@/components/navbar";
 import useFetchOutlets from "./hooks/api/fetch-outlets";
-import Loader from "./components/loader";
+import Loader from "@/components/loader";
+import OutletWrapper from "@/components/outlet-wrapper";
 
 function App() {
   const { outlets, isLoading } = useFetchOutlets();
 
-  if (outlets) console.log(outlets);
+  // if (outlets) console.log(outlets);
 
   return (
     <>
-      <div className="bg-slate-50">
+      <div className="bg-slate-100 ">
         <Navbar />
-
-        {isLoading ? <Loader /> : null}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="max-w-5xl mx-auto">
+            <OutletWrapper outlets={outlets} />
+          </div>
+        )}
       </div>
     </>
   );
