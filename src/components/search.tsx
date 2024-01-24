0e3f48ff4cdde879a175de5c616a9eb0b06e62e9
n-kev1n <kevin_@tuta.io>
2024-01-24 23:07:05 +0100

    added border styles on the search bar

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 const SearchBar = () => {
   return (
-    <div className="hidden md:flex items-center px-2 mx-3 bg-white overflow-hidden rounded-full shadow hover:shadow-lg group transition ">
+    <div className="hidden md:flex items-center px-2 mx-3 bg-white overflow-hidden rounded-full shadow border-[1px] border-slate-200 hover:shadow-lg group transition ">
       <Search className="text-slate-400 w-6 h-6 group-hover:text-slate-600 group-hover:scale-110 transition " />
       <Input
         className="bg-none border-none outline-none ring-0 ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0 w-56"
