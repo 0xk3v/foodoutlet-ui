d4fef2bac32e71c3ee31c36bf289fe3fb71ffbec
n-kev1n <kevin_@tuta.io>
2024-01-30 11:39:49 +0100

    changed image size

--- src/components/outlet-wrapper.tsx
+++ src/components/outlet-wrapper.tsx
@@ -3,7 +3,7 @@ import OutletItem from "./outlet-item";
 
 const OutletWrapper = ({ outlets }: { outlets: Outlet[] }) => {
   return (
-    <div className="w-4/5 h-full mx-auto flex items-center justify-center lg:justify-between flex-wrap">
+    <div className="w-4/5 h-full mx-auto flex items-center flex-wrap">
       {outlets.map((outlet) => (
         <OutletItem key={outlet.id} outlet={outlet} />
       ))}
