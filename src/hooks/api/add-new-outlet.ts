7789b36b5c1153741c716400e7c9d19792bab351
n-kev1n <kevin_@tuta.io>
2024-01-30 11:40:41 +0100

    added new hook to create a new outlet

---
+++ src/hooks/api/add-new-outlet.ts
@@ -0,0 +1,31 @@
+import { baseUrl } from "@/config";
+import { Rating } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+import axios from "axios";
+
+type RequestType = {
+  rating: number;
+  foodOutletId: number;
+};
+
+async function createOutlet(data: RequestType) {
+  const API_URL = `${baseUrl}/api/outlets`;
+  const { data: response } = await axios.post<Rating>(API_URL, data);
+  return response;
+}
+
+function useCreateOutlet() {
+  const { mutate, isSuccess, isError } = useMutation({
+    mutationFn: createOutlet,
+    onSuccess: (data) => {
+      console.log(data);
+      toast.success("A new food outlet has been added!");
+      window.location.href = "http://localhost:5173/";
+    },
+  });
+
+  return { mutate, isSuccess, isError };
+}
+
+export default useCreateOutlet;
