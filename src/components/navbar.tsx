a1c7bd034773002e8580bb26fa1a98fb1aad8ab7
n-kev1n <kevin_@tuta.io>
2024-01-30 07:20:01 +0100

    added new form to add new outlets

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,151 @@
 import Logo from "./logo";
 import SearchBar from "./search";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "./ui/input";
+
+// type Props = {};
+const formSchema = z.object({
+  name: z.string(),
+  address: z.string(),
+  cuisineType: z.string(),
+  openingHours: z.string(),
+  imageUrl: z.string(),
+});
 
 const Navbar = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      address: "",
+      cuisineType: "",
+      openingHours: "",
+      imageUrl: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <div className="w-full h-20 p-2 shadow border-[1px] mb-4 bg-white">
       <div className="flex items-center justify-between container h-full xl:px-30  mx-auto max-w-5xl">
         <Logo />
         <SearchBar />
         <div>
-          <Button className="rounded-full py-1 px-4">Contact us</Button>
+          <Dialog>
+            <DialogTrigger>
+              <Button className="rounded-full py-1 px-4">Add new outlet</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add a new Food outlet</DialogTitle>
+              </DialogHeader>
+              <DialogDescription>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-4"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Outlet name</FormLabel>
+                          <FormControl>
+                            <Input placeholder="name" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="address"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Outlet Address</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Street 123" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="cuisineType"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Cuisine Type</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Italian" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="openingHours"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Opening Hours</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="08:00 AM - 10:00 PM"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="imageUrl"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Image URL</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="https://example.com/italian.jpg"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit" className="w-full">
+                      Add new outlet
+                    </Button>
+                  </form>
+                </Form>
+              </DialogDescription>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </div>
