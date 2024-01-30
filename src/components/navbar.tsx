0702c7a83e5d51e54343be6f986e7d901f075154
n-kev1n <kevin_@tuta.io>
2024-01-30 11:39:19 +0100

    added validation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,17 +21,20 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "./ui/input";
+import useCreateOutlet from "@/hooks/api/add-new-outlet";
 
 // type Props = {};
 const formSchema = z.object({
-  name: z.string(),
-  address: z.string(),
-  cuisineType: z.string(),
-  openingHours: z.string(),
-  imageUrl: z.string(),
+  name: z.string().min(1),
+  address: z.string().min(1),
+  cuisineType: z.string().min(1),
+  openingHours: z.string().min(1),
+  imageUrl: z.string().url({ message: "An image URL should be provided" }),
 });
 
 const Navbar = () => {
+  const { mutate } = useCreateOutlet();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -45,6 +48,7 @@ const Navbar = () => {
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
+    mutate(values);
   }
 
   return (
@@ -52,101 +56,98 @@ const Navbar = () => {
       <div className="flex items-center justify-between container h-full xl:px-30  mx-auto max-w-5xl">
         <Logo />
         <SearchBar />
-        <div>
-          <Dialog>
-            <DialogTrigger>
-              <Button className="rounded-full py-1 px-4">Add new outlet</Button>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Add a new Food outlet</DialogTitle>
-              </DialogHeader>
-              <DialogDescription>
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-4"
-                  >
-                    <FormField
-                      control={form.control}
-                      name="name"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Outlet name</FormLabel>
-                          <FormControl>
-                            <Input placeholder="name" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="address"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Outlet Address</FormLabel>
-                          <FormControl>
-                            <Input placeholder="Street 123" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="cuisineType"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Cuisine Type</FormLabel>
-                          <FormControl>
-                            <Input placeholder="Italian" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="openingHours"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Opening Hours</FormLabel>
-                          <FormControl>
-                            <Input
-                              placeholder="08:00 AM - 10:00 PM"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="imageUrl"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Image URL</FormLabel>
-                          <FormControl>
-                            <Input
-                              placeholder="https://example.com/italian.jpg"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <Button type="submit" className="w-full">
-                      Add new outlet
-                    </Button>
-                  </form>
-                </Form>
-              </DialogDescription>
-            </DialogContent>
-          </Dialog>
-        </div>
+        <Dialog>
+          <DialogTrigger>
+            <Button className="rounded-full py-1 px-4">Add new</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="text-xl">
+                Add a new Food outlet
+              </DialogTitle>
+            </DialogHeader>
+            <DialogDescription>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-4"
+                >
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Outlet name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="name" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="address"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Outlet Address</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Street 123" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="cuisineType"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Cuisine Type</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Italian" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="openingHours"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Opening Hours</FormLabel>
+                        <FormControl>
+                          <Input placeholder="08:00 AM - 10:00 PM" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="imageUrl"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Image URL</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="https://example.com/italian.jpg"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" className="w-full">
+                    Add new outlet
+                  </Button>
+                </form>
+              </Form>
+            </DialogDescription>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
